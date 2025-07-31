548a7dfbe2726c3c16b1670056e1a1e5c314d87c
wangzhen0518 <wangzhen0518@126.com>
2025-07-31 17:04:58 +0800

    feat: deal unknown commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-#[allow(unused_imports)]
+#![allow(dead_code)]
+
 use std::io::{self, Write};
 
+mod utils;
+
 fn main() {
+    utils::config_logger();
+
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => println!("{}: command not found", input.trim()),
+            Err(err) => tracing::error!("{}", err),
+        }
+        input.clear();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
